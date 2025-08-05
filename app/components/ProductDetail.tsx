f503c48f42534be6aec5ab2d3f3ee03ab79e7753
Rafael Strongoli <rafaelstrongoli@gmail.com>
2025-08-04 21:07:01 -0300

    fix:responsive detail view

--- app/components/ProductDetail.tsx
+++ app/components/ProductDetail.tsx
@@ -27,39 +27,39 @@ export default function ProductDetail({
     //agregar modal o aviso con posibilidad de enviar al carrito.
   };
   return (
-    <div className="grid md:grid-cols-2 gap-8 px-40 py-6">
+    <div className="grid md:grid-cols-2 gap-4 md:gap-8 px-4 md:px-40 py-6">
       {/* 📷 Imagen del producto */}
-      <div className="w-full ">
+      <div className="w-full">
         <Image
           src={image || ""}
           alt={name}
-          className="rounded-xl shadow-md object-cover flex justify-center items-center"
+          className="rounded-xl shadow-md object-cover w-full h-auto"
           width={400}
-          height={100}
+          height={400}
         />
       </div>
 
       {/* 🧾 Info del producto */}
       <div className="flex flex-col justify-between space-y-4">
         <div className="flex flex-col gap-4">
-          <h2 className="text-2xl xl:text-4xl font-bold">{name}</h2>
-          <p className="text-gray-600 2xl:text-lg mt-10">
+          <h2 className="text-xl md:text-2xl xl:text-4xl font-bold">{name}</h2>
+          <p className="text-gray-600 text-sm md:text-base 2xl:text-lg mt-4 md:mt-10">
             {model} • {color} • {storage}
           </p>
           {batteryHealth && (
-            <p className="text-sm 2xl:text-lg text-green-600">
+            <p className="text-sm md:text-sm 2xl:text-lg text-green-600">
               🔋 Batería: {batteryHealth}%
             </p>
           )}
           {condition && (
-            <p className="text-sm 2xl:text-base">📦 Estado: {condition}</p>
+            <p className="text-sm md:text-sm 2xl:text-base">📦 Estado: {condition}</p>
           )}
-          <p className="text-lg 2xl:text-4xl font-semibold mt-2">${price}</p>
+          <p className="text-xl md:text-lg 2xl:text-4xl font-semibold mt-2">${price}</p>
           {!inStock && <p className="text-red-500 text-sm">Sin stock</p>}
         </div>
 
         <button
-          className="bg-main text-white py-2 px-4 rounded hover:bg-main/80 transition disabled:bg-gray-400"
+          className="bg-main text-white py-3 md:py-2 px-4 rounded hover:bg-main/80 transition disabled:bg-gray-400 text-base md:text-sm"
           disabled={inStock}
           onClick={handleAddToCart}
         >
